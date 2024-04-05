e09223ff515d18df2fc3486a2c8392e5015b95c7
Francesco Leto <francesco.leto@hubcore.ai>
2024-04-05 15:11:42 +0200

    lint

--- DecoratorPattern/Condiments/Milk.php
+++ DecoratorPattern/Condiments/Milk.php
@@ -5,13 +5,15 @@ namespace App\DecoratorPattern\Condiments;
 use App\DecoratorPattern\Beverage;
 use App\DecoratorPattern\CondimentDecorator;
 
-class Milk extends CondimentDecorator {
+class Milk extends CondimentDecorator
+{
 
 
     protected string $description = 'milk';
     protected float $cost = 0.5;
 
-    public function __construct(Beverage $beverage) {
+    public function __construct(Beverage $beverage)
+    {
         $this->beverage = $beverage;
     }
 
@@ -20,10 +22,8 @@ class Milk extends CondimentDecorator {
         return "{$this->beverage->getDescription()} + {$this->description}";
     }
 
-    public function cost (): float
+    public function cost(): float
     {
         return $this->beverage->cost() + $this->cost;
-
     }
-
 }
--- DecoratorPattern/Condiments/Sugar.php
+++ DecoratorPattern/Condiments/Sugar.php
@@ -5,22 +5,22 @@ namespace App\DecoratorPattern\Condiments;
 use App\DecoratorPattern\Beverage;
 use App\DecoratorPattern\CondimentDecorator;
 
-class Sugar extends CondimentDecorator {
+class Sugar extends CondimentDecorator
+{
 
 
     protected string $description = 'sugar';
     protected float $cost = 0.21;
 
-    public function __construct(Beverage $beverage) {
+    public function __construct(Beverage $beverage)
+    {
         $this->beverage = $beverage;
     }
 
-    
 
-    public function cost (): float
+
+    public function cost(): float
     {
         return $this->beverage->cost() + $this->cost;
-
     }
-
 }
